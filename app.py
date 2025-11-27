a2aa2bda451d0a534b5ff6621e5af3f5702eb525
SRPVT <habibi786123r@gmail.com>
2025-11-27 14:28:13 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-# Web server for keeping the Discord bot alive
-# Import the Flask app from keep_alive.py
-from keep_alive import app
-
-# This file is used by gunicorn to serve the web application
-if __name__ == "__main__":
-    # This code won't run under gunicorn, but can be used for local testing
-    app.run(host="0.0.0.0", port=5000)
